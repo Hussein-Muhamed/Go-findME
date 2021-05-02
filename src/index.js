606173bb6afbc83808e1b32f8372d9a293d301ca
Hussein <helnemar@yahoo.com>
2021-05-02 09:20:40 +0200

    Inital commit

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const request = require('request')
+const express = require('express')
+require('./db/mongoose')
+const User = require('./models/user')
+const path = require('path')
+
+const port = process.env.PORT || 3000
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
+
+app.use(express.json())
+
+// to login user
+{
+    app.post('/users/login',async (req, res)=>{
+        try{
+            const user = await User.findByCredentials(req.body.email, req.body.password)
+            res.status(201).send(user)
+        }catch (e){
+            // console.log(e)
+            res.status(400).send()
+        }   
+    })
+}
+
+// to create new user 
+app.post('/user', async(req, res)=>{
+    const user =  new User(req.body)
+    try{
+        await user.save()
+        res.status(201).send(user)
+    } catch(e){
+        res.status(400).send(e)
+    }
+})
+
+// to read users
+app.get('/users', async (req, res)=>{
+    try{
+        const users = await User.find({})
+        res.status(200).send(users)
+    } catch(e){
+        res.status(500).send(e)
+    }
+})
+
+app.listen(port , ()=>{
+    console.log(`The server connection on port ${port}`)
+})
---
+++ src/models/user.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema({
+    fname:{
+        type: String,
+        required: true,
+    }, lname:{
+        type: String,
+        required: true,
+    }, email:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value))
+                throw new error('email is unvalid')
+        }
+    }, password:{
+        type: String,
+        minLength: 7,
+        required: true,
+    }, age:{
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
+            }
+        }
+    }
+})
+
+// to hash the password 
+userSchema.pre('save',async function(next) {
+    const user = this 
+    if(user.isModified('password'))
+        user.password = await bcrypt.hash(user.password, 8)
+    next()
+})
+
+// login check
+userSchema.statics.findByCredentials = async (email, password) =>{
+    const user = await User.findOne({email})
+    if(!user)
+        throw new Error('Uncorrect email')
+    const ismatch = await bcrypt.compare(password, user.password)
+    if(!ismatch)
+        throw new Error('Uncorrect password')
+    return user
+}
+
+const User = mongoose.model('Users', userSchema)
+
+module.exports = User
