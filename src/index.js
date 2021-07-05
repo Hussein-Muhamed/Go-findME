2d566c1cfcf8f61345abce25bf2ba7f2cddd9fe6
Hossam Samir <techiiestan@gmail.com>
2021-07-05 05:31:37 +0200

    chat

--- src/index.js
+++ src/index.js
@@ -4,28 +4,22 @@ const cors = require('cors')
 require('./db/mongoose')
 const User = require('./models/user')
 const Post = require('./models/post')
-const Message = require('./models/messages')
 const Trusted = require('./models/trustedPeople')
-const Notification = require('./models/notification')
 const path = require('path')
 const userrouter = require('./routers/user')
 const postrouter = require('./routers/post')
-const messagerouter = require('./routers/messages')
 const trustedrouter = require('./routers/trustedPeolple')
-const notificationrouter = require('./routers/notification')
 const auth = require('./middleware/auth')
 const validator = require('validator')
 // const {sendWelcomeEmail, sendCancelationEmail} = require('./emails/account')
 const bodyParser = require("body-parser");
+const port = process.env.PORT
+const app = require('express')();
 
-
-
-const port = process.env.PORT 
-const app = express()
 
 app.use(cors())
-// const viewPath = path.join(__dirname, '../public/css')
-// const stylePath = path.join(__dirname, '../public/html')
+// const viewPath = path.join(dirname, '../public/css')
+// const stylePath = path.join(dirname, '../public/html')
 // app.use(express.static(viewPath))
 // app.use(express.static(stylePath))
 
@@ -33,12 +27,24 @@ app.use(express.json())
 app.use(userrouter)
 app.use(postrouter)
 app.use(trustedrouter)
-app.use(messagerouter)
-app.use(notificationrouter)
 app.use(bodyParser.json());
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
-app.listen(port , ()=>{
-    console.log(`The server connection on port ${port}`)
-})
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "http://localhost:3001/",
+    }
+});
+io.on('connection', (socket) => {
+    console.log("new user connected");
+    socket.on('disconnect', () => {
+        console.log("new user disconnected");
+    });
+    socket.on('new_message', (data) => {
+        console.log("new message !!!");
+        socket.broadcast.emit('msgFromServer', data);
+    });
+});
 
+server.listen(3000);
