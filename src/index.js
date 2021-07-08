e5be936a686040457abe6c4c7018c622abe45635
Hussein <helnemar@yahoo.com>
2021-07-09 01:54:29 +0200

    update

--- src/index.js
+++ src/index.js
@@ -12,21 +12,21 @@ const trustedrouter = require('./routers/trustedPeolple')
 const auth = require('./middleware/auth')
 const validator = require('validator')
 const bodyParser = require("body-parser");
-<<<<<<< HEAD
+// <<<<<<< HEAD
 const app = require('express')();
 
 app.use(cors())
-=======
-const port = process.env.PORT
-const app = require('express')();
+// =======
+// const port = process.env.PORT
+// const app = require('express')();
 
 
 app.use(cors())
-// const viewPath = path.join(dirname, '../public/css')
-// const stylePath = path.join(dirname, '../public/html')
-// app.use(express.static(viewPath))
-// app.use(express.static(stylePath))
->>>>>>> 2d566c1cfcf8f61345abce25bf2ba7f2cddd9fe6
+// // const viewPath = path.join(dirname, '../public/css')
+// // const stylePath = path.join(dirname, '../public/html')
+// // app.use(express.static(viewPath))
+// // app.use(express.static(stylePath))
+// >>>>>>> 2d566c1cfcf8f61345abce25bf2ba7f2cddd9fe6
 
 app.use(express.json())
 app.use(userrouter)
@@ -41,10 +41,10 @@ const io = require('socket.io')(server, {
         origin: "http://localhost:3001/",
     }
 });
-<<<<<<< HEAD
+// <<<<<<< HEAD
 
-=======
->>>>>>> 2d566c1cfcf8f61345abce25bf2ba7f2cddd9fe6
+// =======
+// >>>>>>> 2d566c1cfcf8f61345abce25bf2ba7f2cddd9fe6
 io.on('connection', (socket) => {
     console.log("new user connected");
     socket.on('disconnect', () => {
@@ -56,9 +56,9 @@ io.on('connection', (socket) => {
     });
 });
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 server.listen(3000);
 console.log(' 🔥 Port is connected in 3000 port! 🔥🚀')
-=======
-server.listen(3000);
->>>>>>> 2d566c1cfcf8f61345abce25bf2ba7f2cddd9fe6
+// =======
+// server.listen(3000);
+// >>>>>>> 2d566c1cfcf8f61345abce25bf2ba7f2cddd9fe6
